92c793e849bd4ef198170163838cbc5c90ee3d70
Axemoth <Rushil.Gorasia@gmail.com>
2026-02-08 15:58:33 +0530

    Implement coffee price calculation logic

--- src/07-coffee-shop.js
+++ src/07-coffee-shop.js
@@ -31,5 +31,39 @@
  * @returns {number} Total price or -1 for invalid input
  */
 export function calculateCoffeePrice(size, type, extras = {}) {
-  // Your code here
+  let number = 0;
+  if(size!== "small" && size!=="medium" && size!=="large"){
+    return -1
+  }
+  if(size === "small"){
+    number = 3 
+  }
+  else if(size === "medium"){
+    number = 4
+  }
+  else if(size === "large"){
+    number = 5
+  }
+  if(type !== "regular" && type !== "latte" && type!== "cappuccino" && type!== "mocha"){
+    return -1
+  }
+  if(type === "regular"){
+    number += 0
+  }
+  if(type === "latte"){
+    number += 1
+  }
+  if(type === "cappuccino"){
+    number += 1.5
+  }
+  if(type === "mocha"){
+    number += 2
+  }
+  if(extras.whippedCream){
+    number += 0.5
+  }
+  if(extras.extraShot){
+    number += 0.75
+  }
+  return number
 }
